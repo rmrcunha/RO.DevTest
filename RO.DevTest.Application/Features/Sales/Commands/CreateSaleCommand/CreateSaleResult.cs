3993ee38fdde90fb175bc3c1cea7884d8157d1a4
Renan Cunha <renancdev@gmail.com>
2025-04-24 11:17:36 -0300

    dev: paginacao das vendas

--- RO.DevTest.Application/Features/Sales/Commands/CreateSaleCommand/CreateSaleResult.cs
+++ RO.DevTest.Application/Features/Sales/Commands/CreateSaleCommand/CreateSaleResult.cs
@@ -10,7 +10,7 @@ public class CreateSaleResult
 {
     public string SaleId { get; set; } = string.Empty;
     public string ProductId { get; set; } = string.Empty;
-    public string ProductName { get; set; } = string.Empty;
+    public Domain.Entities.Product? Product { get; set; }
     public int Quantity { get; set; } = 0;
     public double TotalPrice { get; set; } = 0.0;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
@@ -19,6 +19,7 @@ public class CreateSaleResult
     {
         SaleId = sale.Id;
         ProductId = sale.ProductId;
+        Product = sale.Product;
         Quantity = sale.Quantity;
         TotalPrice = sale.TotalPrice;
         CreatedAt = sale.CreatedAt;
---
+++ RO.DevTest.Application/Features/Sales/DTO/SaleDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RO.DevTest.Application.Features.Sales.DTO;
+
+public class SaleDTO
+{
+    public int Id { get; set; }
+    public string? ProductName { get; set; }
+    public string? CustomerName { get; set; }
+    public decimal Price { get; set; }
+    public DateTime SaleDate { get; set; }
+}
